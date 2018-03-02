4b930c95ec7e4b3dde2c1f664b3bc668ae87650a
Edgar Hernandez <edgarchortiz@gmail.com>
2018-03-02 13:16:53 -0800

    Refactor form in new-term.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,31 @@ import { Term } from './term.model';
 @Component({
   selector: 'app-root',
   template: `
-    <new-term (newTermSender) = "addTerm($event)"></new-term>
-    <term-list [childTermList]="masterTermList"></term-list>
+  <header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Terminology</a>
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="https://www.codecademy.com/articles/glossary-html">HTML Glossary</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.codecademy.com/articles/glossary-css">CSS Glossary</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://www.oracle.com/technetwork/java/glossary-135216.html">Java Glossary</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">JavaScript Resource</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+    <div class="container">
+      <new-term (newTermSender) = "addTerm($event)"></new-term>
+      <term-list [childTermList]="masterTermList"></term-list>
+    </div>
   `
 })
 
--- app/new-term.component.ts
+++ app/new-term.component.ts
@@ -4,6 +4,8 @@ import { Term } from './term.model';
 @Component({
   selector: 'new-term',
   template: `
+  <h3>Create new flashcard</h3>
+  <p>Enter term, definition and select which catergory to storage the term.<p>
   <form id="newTermForm">
     <div class="form-group">
       <div class="col-md-4">
@@ -14,15 +16,15 @@ import { Term } from './term.model';
       </div>
     </div>
     <div class="col-sm-4">
-     <label for="inputState">Language</label>
-     <select #newLanguage class="form-control">
-       <option [value]="HTML">HTML</option>
-       <option [value]="CSS">CSS</option>
-       <option [value]="Java">Java</option>
-       <option [value]="JavaScript">JavaScript</option>
+     <select #newLanguage class="custom-select custom-select-lg mb-3">
+      <option selected>Choose category</option>
+      <option [value]="HTML">HTML</option>
+      <option [value]="CSS">CSS</option>
+      <option [value]="Java">Java</option>
+      <option [value]="JavaScript">JavaScript</option>
      </select>
     </div>
-    <button class="btn btn-primary" (click)="submitForm(newTerm.value, newDefinition.value, newLanguage.value); newTerm.value=''; newDefinition.value=''; newLanguage.value='';">Submit</button>
+    <button class="btn btn-outline-primary" id =newTermButton (click)="submitForm(newTerm.value, newDefinition.value, newLanguage.value); newTerm.value=''; newDefinition.value=''; newLanguage.value='';">Submit</button>
   </form>
   `
 })
